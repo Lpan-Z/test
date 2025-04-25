dadb6ec4de02aa643ce6a5f76d27c74b6fdc5138
Lpan-Z <1270732854@qq.com>
2025-04-25 20:40:54 +0800

    Delete face directory

--- face/src/main/java/FaceApp/java/constant/Constant.java
+++
@@ -1,6 +0,0 @@
-package FaceApp.java.constant;
-
-public class Constant {
-    public static final String ACCESS_KEY_ID = "LTAI5tRYJR7KHVkbXF8LiLR7";
-    public static final String ACCESS_KEY_SECRET = "7uigDmo6S3qtsXnq8lfohDc69uhcme";
-}
